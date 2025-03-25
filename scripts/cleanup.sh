b82be3affc162ffb1f46883f62e3ea3f28242696
Santiago Josa <santiago.josa@oncosur.org>
2025-03-25 14:01:04 +0100

    Limpia entorno de trabajo. Backup para hacer prueba (cleanup.sh)

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # Elimina los archivos de las carpetas indicadas
 
-if [ $# -eq 0 ]; then
-    echo "Limpiando directorio de trabajo"
-    echo $PWD
+shopt -u dotglob # Mantengo archivos y carpetas ocultos (por ejemplo carpetas git)
+shopt -s extglob # Habilito el uso de patrones extendidos
 
-    #rm -r $PWD -v
-else
-    for dir in $@
-    do
-        echo "Limpiando $dir"
-        echo $dir/*
-        #rm -r $dir/* -v
-    done
+if [ $# -eq 0 ] # Elimina todo el directorio de trabajo
+then
+    echo "Limpiando directorio de trabajo"
+    rm -rv "$PWD"/*
+    exit 0
 fi
+
+
+for dir in "$@" # Para cada directorio pasado como argumento
+do
+    if [ "$dir" = "data" ] # Si el directorio es data
+    then
+        echo "Limpiando $dir excepto $dir/urls"
+        rm -rv "$dir"/!(urls)
+    elif [ "$(ls "$dir")" ] # Si el directorio no esta vacio
+    then
+        rm -rv "$dir"/*
+    else # Si el directorio esta vacio
+        echo "$dir esta vacio"
+    fi
+done
