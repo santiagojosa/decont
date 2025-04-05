f91762cae3dd60e3f07a5b8e78bac83fd0c591d7
Santiago Josa <santiago.josa@oncosur.org>
2025-04-05 14:40:12 +0200

    Quito la redireccion a stderr cuando falla md5sum. Redirijo mensaje de error a una variable de entorno en (download.sh) que se comprueba luego en (pipeline.sh)

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -25,13 +25,11 @@ contaminants_filter="$3"
 
 #Download all the files specified in data/filenames
 echo "➡️ ➡️  Descargando archivos fastq.gz..."
-tmp_err=$(mktemp log/stderr_download_XXXXXX) #Variable temporal en carpeta log para almacenar errores
-#trap 'rm -f "$tmp_err"' EXIT INT TERM #Borra el archivo temporal al salir del script, haya error o no, o si detengo el script
-xargs -a "$input_urls" -I {} bash scripts/download.sh {} data 2>> "$tmp_err" #Si hay error (md5sum erroneo, por ejemplo), se guarda en stderr en tmp_err
-#xargs lee el archivo linea por linea y ejecuta el script metiendo en {} cada url
-if grep -q 'Error en el MD5 check' "$tmp_err"; then #Comprueba si el archivo temporal tiene contenido, que sera la salida de error
-  echo "❌❌ Se produjeron errores al descargar archivos. Revisa si hay mensajes de error en las lineas previas."
-  #cat "$tmp_err"
+trap 'rm -f "$TMP_ERR"' EXIT INT TERM #Borra el archivo temporal al salir del script, haya error o no, o si detengo el script
+xargs -a "$input_urls" -I {} bash scripts/download.sh {} data #xargs lee el archivo linea por linea y ejecuta el script metiendo en {} cada url
+if grep -q 'Error en el MD5 check' "$TMP_ERR"; then #Comprueba si el archivo temporal tiene contenido, que sera la salida de error
+  echo "❌❌ Se produjeron errores al descargar archivos. Error encontrado:"
+  cat "$TMP_ERR"
   exit 1
 fi
 printf -- '=%.0s' {1..150}; printf "\n\n"
